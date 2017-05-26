7174248509e18bb6f654cf7a80264161aa0270c3
王文宇 <wangwenyu9394@gmai.com>
2017-05-27 00:06:10 +0800

    use ES6 redo app.js

---
+++ generators/app/index.js
@@ -0,0 +1,94 @@
+'use strict';
+
+// 加载 yeoman generator 的核心功能模块.
+const Generator = require('yeoman-generator');
+// 加载文件读写模块.
+const fs = require('fs');
+// 加载路径模块
+const path = require('path');
+// 加载输出模块
+const chalk = require('chalk');
+//加载yeoman语言输出模块
+const yosay = require('yosay');
+
+/**
+ * Base Module
+ */
+module.exports = class extends Generator{
+
+	constructor(args,opts){
+		super(args,opts);
+
+	    this.name = path.basename(process.cwd());
+	    this.author = "Jacobwang";
+	    this.description = '';
+	}
+
+	initializing() {
+	    this.log(yosay('欢迎使用全网数商前端 ' + chalk.red('QWUI') + ' 项目生成器!'));
+	}
+
+	// prompting(){
+	// 	return this.prompt([{
+ //                type: 'input',
+ //                name: 'name',
+ //                message: '请输入项目名称:',
+ //                default: this.name
+ //            }, {
+ //                type: 'input',
+ //                name: 'description',
+ //                message: '请输入项目描述:',
+ //                default: this.description
+ //            }, {
+ //                type: 'input',
+ //                name: 'author',
+ //                message: '请输入开发人员:',
+ //                default: this.author
+ //            }]).then((answers) => {
+
+	//       	  this.name = answers.name;
+ //              this.pkgName = answers.name;
+ //              this.author = answers.author;
+ //              this.description = answers.description;
+	//     });
+	// }
+
+
+	writing(){
+		let data = {
+			name:this.name,
+			description:this.description,
+			author:this.author
+		}
+
+		let _self = this;
+
+		fs.mkdirSync('dist');
+
+	    this.spawnCommand('git', ['clone', 'https://github.com/wenyuking/qwui.git'])
+	    	.on('exit',function(){
+	    		_self.spawnCommand('rm', ['-rf','qwui/.git'])
+	    	})
+
+	    this.fs.copyTpl(
+	      this.templatePath('package.json'),
+	      this.destinationPath('package.json'),
+	      data
+	    );
+
+	    this.fs.copy(
+	      this.templatePath("gulpfile.js"),
+	      this.destinationPath("gulpfile.js")
+	    );
+
+	}
+
+	install() {
+	   this.installDependencies({
+	      npm: true,
+	      callback: function() {
+	        this.spawnCommand('gulp', ['qwui']);
+	      }.bind(this)
+	    });
+	}
+}
---
+++ generators/app/index1.js
@@ -0,0 +1,91 @@
+'use strict';
+
+var generators = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+var path = require('path');
+
+module.exports = generators.Base.extend({
+    initializing: function() { //初始化准备工作
+
+    },
+    prompting: function() {
+        var done = this.async();
+
+        this.log(yosay('欢迎使用全网数商前端 ' + chalk.red('QWUI') + ' 项目生成器!'));
+        this.name = path.basename(process.cwd());
+        this.description = '';
+        this.author = '';
+
+        var prompts = [{
+                type: 'input',
+                name: 'name',
+                message: '请输入项目名称:',
+                default: this.name
+            }, {
+                type: 'input',
+                name: 'description',
+                message: '请输入项目描述:',
+                default: this.description
+            }, {
+                type: 'input',
+                name: 'author',
+                message: '请输入开发人员:',
+                default: this.author
+            }
+
+        ];
+
+        this.prompt(prompts, function(props) {
+
+            this.name = props.name;
+            this.pkgName = props.name;
+            this.author = props.author;
+            this.description = props.description;
+
+            done();
+        }.bind(this));
+    },
+    writing: {
+        app: function() {
+            var done = this.async();
+            this.template('package.json', 'package.json');
+            this.template('gulpfile.js', 'gulpfile.js');
+
+            this.spawnCommand('git', ['clone', 'https://github.com/wenyuking/qwui.git'])
+                .on('exit', function(code) {
+                    if (code) {
+                        done(new Error('code:' + code));
+                    } else {
+                        done();
+                    }
+                })
+                .on('error', done);
+        }
+    },
+    install: function() {
+        var done = this.async();
+        this.spawnCommand('cnpm', ['install']) //安装项目依赖
+            .on('exit', function(code) {
+                if (code) {
+                    done(new Error('code:' + code));
+                } else {
+                    done();
+                }
+            })
+            .on('error', done);
+    },
+    end: function() {
+        var done = this.async();
+        this.spawnCommand('gulp', ['qwui']) //生成器退出前运行gulp，开启watch任务
+            .on('exit', function(code) {
+                if (code) {
+                    done(new Error('code:' + code));
+                } else {
+                    done();
+                }
+            })
+            .on('error', done);
+    }
+
+})
